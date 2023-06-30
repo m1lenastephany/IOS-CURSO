b4d5e58965487b3f4968bc9b13369cff34cf950e
m1lenastephany <milena_stephany@live.com>
2023-06-29 21:21:29 -0300

    tcc

--- milena stephany/WORKSPACE/trabalho/teste/script.js
+++ milena stephany/WORKSPACE/trabalho/teste/script.js
@@ -1,23 +1,42 @@
-function playVideo(source) {
-    var video = document.getElementById('video');
-    video.src = source;
-    video.load();
-    video.play();
-  }
-  $(document).ready(function() {
-    $('.slick-slider').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-      nextArrow: '<button type="button" class="slick-next">Next</button>',
-    });
-  });
-  
-  function playVideo(source) {
-    var video = document.getElementById('video');
-    video.src = source;
-    video.load();
-    video.play();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
   }
 
- 
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Chamar a função showSlides automaticamente a cada 5 segundos
+setInterval(function () {
+  plusSlides(1);
+}, 5000);
